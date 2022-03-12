e4220c2ff92687040fb295d302e9cf7225850ec0
jyb <jyb@123.com>
2022-03-12 18:52:18 +0800

    update UserMapper0

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -60,12 +60,12 @@ public interface UserMapper {
 
 
 
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
-    public void test7();
+    public void test10();
+    public void test20();
+    public void test30();
+    public void test40();
+    public void test50();
+    public void test60();
+    public void test70();
 
 }
